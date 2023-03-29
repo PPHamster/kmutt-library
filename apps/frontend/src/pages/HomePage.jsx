e1b47abaad1ef56f5071074ade472b7d89ff2da4
PPHamster <shinnapatjr@gmail.com>
2023-03-29 21:40:47 +0700

    refactor: test section hero navbar

--- apps/frontend/src/pages/HomePage.jsx
+++ apps/frontend/src/pages/HomePage.jsx
@@ -1,26 +1,10 @@
-import { popup } from '@/utils/Popup';
-
 export const HomePage = () => {
-  const onBtnClick = () => {
-    popup.fire({
-      imageUrl: 'https://img1.picmix.com/output/stamp/normal/9/8/1/6/1776189_c45b6.gif',
-      title: 'Bili Bili Guba!!',
-      imageAlt: 'Gawr Gura Gif'
-    })
-  };
 
   return (
     <>
-      <section className="px-[30px] py-[15px] bg-slate-800 min-h-[100vh] font-sans">
-        <div className="max-w-[700px] mx-auto text-center">
-          <div className="text-white text-center text-6xl">Is Gubaaa!!</div>
-          <img src="https://img1.picmix.com/output/stamp/normal/2/3/4/4/1774432_0219c.gif" className="mx-auto" />
-          <button
-            className="w-[150px] h-[45px] rounded-md border-black border-2 bg-cyan-100 hover:bg-cyan-600 font-bold text-2xl my-[20px] ease-out duration-300"
-            onClick={onBtnClick}
-          >Click ?</button>
-        </div>
-      </section>
+      <div className="bg-library min-h-[100vh] bg-cover opacity-75">
+        
+      </div>
     </>
   )
 };
--- apps/frontend/tailwind.config.cjs
+++ apps/frontend/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "library": "url('https://lh3.googleusercontent.com/p/AF1QipNQ3ArxNY1PVrTXdcaUfQBv95pA9iC9DonJO6Hb=w600-k')"
+      }
+    },
   },
   plugins: [],
 }
