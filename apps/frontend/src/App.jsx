b51e9ee761be1147f88aa8b7e986a27811ce317c
Akkarachai6787 <akkarachai6787@gmail.com>
2023-04-05 20:48:41 +0700

    event more

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage';
+import { EventPage } from './pages/EventPage';
 
 export const App = () => {
   return (
     <BrowserRouter basename='/'>
       <Routes>
         <Route path='/' element={ <HomePage /> } />
+        <Route path='/event' element={ <EventPage /> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ apps/frontend/src/components/Event/JoinEvent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function JoinEvent() {
+    return (
+        <div className="flex flex-row w-full h-[700px] ml-[15vw] mt-[9rem]">
+            <div className="flex flex-col">
+                <h1 className='font-bold font-roboto text-5xl text-[#454545] ml-[30px]'>เข้าร่วมกิจกรรม</h1>
+            </div>
+        </div>
+    )
+}
